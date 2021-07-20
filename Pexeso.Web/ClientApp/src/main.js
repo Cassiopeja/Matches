e91362b9fd78999bde704df1e26b76a0e5b14a4c
Oksana Komarova <3198753+Cassiopeja@users.noreply.github.com>
2021-07-20 12:23:23 +0300

    Installed vuex.

--- Pexeso.Web/ClientApp/src/main.js
+++ Pexeso.Web/ClientApp/src/main.js
@@ -5,6 +5,7 @@ import router from './router'
 import Axios from 'axios';
 import gameHubPlugin from "@/plugins/gameHubPlugin";
 import Notification from "vue-notification";
+import store from './store'
 
 Vue.use(gameHubPlugin);
 Vue.use(Notification);
@@ -15,11 +16,12 @@ new Vue({
   vuetify,
   router,
   render: h => h(App),
+  store,
+
   created() {
     this.$http.interceptors.request.use(config => {
       config.baseURL = `${process.env.VUE_APP_BACKEND_API}/api`;
       return config;
     });
   }
-
 }).$mount('#app')
