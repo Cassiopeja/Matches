fcd40268b32852b7a7c2de9acd5857386d852778
Oksana Komarova <3198753+Cassiopeja@users.noreply.github.com>
2021-07-26 13:22:09 +0300

    Fixed error in nav bar. On click didn't work properly.

--- Pexeso.Web/ClientApp/src/components/Navbar.vue
+++ Pexeso.Web/ClientApp/src/components/Navbar.vue
@@ -1,36 +1,33 @@
 <template>
-  <v-app-bar 
-      app
-      color="blue darken-4"
-      dark
-    >
+  <v-app-bar app color="blue darken-4" dark>
     <v-app-bar-title class="text-uppercase text--white">
-      <span
-          @click="$router.push('/').catch(() => {})"
-          style="cursor: pointer"
-      >
-          PEXESO
+      <span style="cursor: pointer" @click="onGoHome">
+        PEXESO
       </span>
     </v-app-bar-title>
-    <v-spacer/>
+    <v-spacer />
     <player-avatar v-if="currentPlayer" :player="currentPlayer"></player-avatar>
   </v-app-bar>
-
 </template>
 
 <script>
 import PlayerAvatar from "@/components/PlayerAvatar";
-import {mapGetters} from "vuex";
+import { mapGetters } from "vuex";
 
 export default {
   name: "Navbar",
-  components: {PlayerAvatar},
+  components: { PlayerAvatar },
+  methods: {
+    onGoHome() {
+      this.$router.push("/").catch(e => {
+        console.error(e);
+      });
+    }
+  },
   computed: {
-    ...mapGetters(['currentPlayer'])
+    ...mapGetters(["currentPlayer"])
   }
-}
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
