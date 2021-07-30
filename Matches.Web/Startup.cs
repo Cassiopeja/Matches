938d6604cfde55c64c70ac27df642104ac5532df
Oksana Komarova <3198753+Cassiopeja@users.noreply.github.com>
2021-07-30 16:44:45 +0300

    Removed Cors policy.

--- Matches.Web/Startup.cs
+++ Matches.Web/Startup.cs
@@ -44,14 +44,12 @@ namespace Matches
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseCors("AllowAny");
             }
             else
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
-                app.UseCors("AllowAny");
             }
 
             if (!env.IsDevelopment()) app.UseHttpsRedirection();
